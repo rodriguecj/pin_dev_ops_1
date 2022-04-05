80d9b65915f959384ef2925f82ae9c152090beb3
JR <jrodriguez@queplan.cl>
2022-04-04 21:07:46 -0400

    Angular Title was changed

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-app de PIN 2';
+  title = 'Angular-app de PIN 2';
 }
