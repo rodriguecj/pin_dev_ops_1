04808e3c4f201ea01a1772c4cb9d91bc8e6c3007
JR <jrodriguez@queplan.cl>
2022-04-04 22:30:49 -0400

    Test WebHook

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Angular-app de PIN 2022';
+  title = '2022 Angular-app de PIN 2022';
 }
